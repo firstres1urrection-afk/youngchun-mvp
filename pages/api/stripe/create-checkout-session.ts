b6473e8e3d0a1c097a763960686cd1c2d4064d8e
firstres1urrection-afk <firstres1urrection@gmail.com>
2025-12-16 20:53:41 +0900

    Update create-checkout-session.ts

--- pages/api/stripe/create-checkout-session.ts
+++ pages/api/stripe/create-checkout-session.ts
@@ -2,7 +2,6 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2022-11-15',
 });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
